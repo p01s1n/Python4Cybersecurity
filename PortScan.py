bca8a6d78233f2af08dfc428c49d56401dca15c5
Iverson Kester <Iverson.dk23@gmail.com>
2024-02-09 21:34:00 -0800

    Added AckScan & XmasScan

--- PortScan.py
+++ PortScan.py
@@ -2,9 +2,10 @@ import ipaddress
 from scapy.layers.dns import *
 from scapy.layers.inet import *
 
-ports = [22,25,80,110,53,443,445,8080,8443]
+ports = [25,80,53,443,445,8080,8443]
 
 def SynScan(host):
+    print("\nSYN Scan Results:")
     ans,unans = sr(
         IP(dst=host)/
         TCP(sport=33333,dport=ports,flags="S")
@@ -13,18 +14,49 @@ def SynScan(host):
     for (s,r) in ans:
         if s[TCP].dport == r[TCP].sport and r[TCP].flags=="SA":
             print(s[TCP].dport)
-
 #Python for cybersecurity Chapter 1 Suggested Exercises #1
+    print("Closed ports at %s:" % host)
+    for (s,r) in ans:
+        if s[TCP].dport == r[TCP].sport and r[TCP].flags=="RA":
+            print(r[TCP].sport)
+    print("Filtered ports at %s:" % host)
+    for (s) in unans:
+        print(s[TCP].dport)
+    print()
+
+#Suggested exercises #2
+def AckScan(host):
+    print("ACK Scan Results:")
+    ans,unans = sr(
+        IP(dst=host)/
+        TCP(sport=33333,dport=ports,flags="A")
+        ,timeout=2,verbose=0)
     print("Filtered ports at %s:" % host)
     for (r) in unans:
-        if r[TCP].flags=="S":
+        print(r[TCP].dport)
+    print("Unfiltered ports at %s:" % host)
+    for (s,r) in ans:
+        if s[TCP].dport == r[TCP].sport and r[TCP].flags=="RA":
             print(r[TCP].dport)
-    print("Closed ports at %s:" % host)
+    print()
+
+def XmasScan(host):
+    print("XMAS Scan Results:")
+    ans,unans = sr(
+    IP(dst=host)/
+    TCP(sport=33333,dport=ports,flags="FPU")
+    ,timeout=2,verbose=0)
+    print("Open|Filtered ports at %s:" % host)
     for (r) in unans:
-        if r[TCP].flags=="SR":
+        print(r[TCP].dport)
+    print("Closed ports at %s:" % host)
+    for (s,r) in ans:
+        if s[TCP].dport==r[TCP].sport and r[TCP].flags=="":
             print(r[TCP].dport)
+    print()
 
 def DNSScan(host):
+    print("DNS Scan Results:")
     ans,unans = sr(
         IP(dst=host)/
         UDP(dport=53)/
@@ -32,6 +64,7 @@ def DNSScan(host):
         ,timeout=2,verbose=0)
     if ans and ans[UDP]:
         print("DNS Server at %s"%host)
+    print()
     
 host = input("Enter IP Address: ")
 try:
@@ -41,4 +74,6 @@ except:
     exit(-1)
 
 SynScan(host)
+AckScan(host)
+XmasScan(host)
 DNSScan(host)
